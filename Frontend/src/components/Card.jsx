808f6ef2de0e4569f572af8a794a5b90c5d3eac6
satyam singh <satyamweb2004@gmail.com>
2025-07-13 23:58:36 +0530

    Update Card.jsx

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { UserDataContext } from "../context/userContext";
+import { UserDataContext } from "../context/UserContext";
 
 const Card = ({ image }) => {
   const {
